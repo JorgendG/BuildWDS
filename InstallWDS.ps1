5a1e011536e29e30e7cfbf0d0e2ad7e69ef78e25
Jorgen de Gier <jorgen.de.gier@salomon-it.nl>
2019-12-28 21:51:58 +0100

    sneller

--- InstallWDS.ps1
+++ InstallWDS.ps1
@@ -5,24 +5,27 @@ start-transcript -path c:\windows\temp\installwds.txt
 $sourcedir = "\\nasje\public\wim"
 
 New-Item -ItemType Directory -Path c:\ -Name WDSImages
+<#
 Copy-Item -Path "$sourcedir\boot.wim" -Destination c:\WDSImages
 Copy-Item -Path "$sourcedir\installw10_19h2.wim" -Destination c:\WDSImages
 Copy-Item -Path "$sourcedir\install2019.wim" -Destination c:\WDSImages
 Copy-Item -Path "$sourcedir\install2016.wim" -Destination c:\WDSImages
 Copy-Item -Path "$sourcedir\install2012r2.wim" -Destination c:\WDSImages
 Copy-Item -Path "$sourcedir\unattended.xml" -Destination c:\WDSImages
+#>
 Add-WindowsFeature WDS -includeall
 & wdsutil.exe /initialize-server /reminst:'c:\remoteinstall' /standalone 
 New-WdsInstallImageGroup -Name 'Windows 10' 
 New-WdsInstallImageGroup -Name 'Windows Server 2019' 
 New-WdsInstallImageGroup -Name 'Windows Server 2016' 
 New-WdsInstallImageGroup -Name 'Windows Server 2012R2'
+<#
 Import-WdsBootImage -Path c:\wdsimages\boot.wim -NewDescription 'W2K16 WDS Boot'
 Import-WdsInstallImage -Path c:\wdsimages\installw10_19h2.wim -ImageName 'Windows 10 Enterprise Evaluation' -ImageGroup 'Windows 10'
 Import-WdsInstallImage -Path c:\wdsimages\install2019.wim -ImageName 'Windows Server 2019 SERVERSTANDARD' -ImageGroup 'Windows Server 2019'
 Import-WdsInstallImage -Path c:\wdsimages\install2016.wim -ImageName 'Windows Server 2016 SERVERSTANDARD' -ImageGroup 'Windows Server 2016'
 Import-WdsInstallImage -Path c:\wdsimages\install2012r2.wim -ImageName 'Windows Server 2012 R2 SERVERSTANDARD' -ImageGroup 'Windows Server 2012R2'
-    
+#>    
 & wdsutil.exe /Set-Server /AnswerClients:All
 
 Install-PackageProvider -Name "Nuget" -Force
