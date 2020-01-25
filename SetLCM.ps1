af1d24b2bdb287fd54fbe1c25ea7bd9d4234852a
Jorgen de Gier <jorgen.de.gier@salomon-it.nl>
2020-01-25 22:40:58 +0100

    added setlcm

---
+++ SetLCM.ps1
@@ -0,0 +1,21 @@
+[dsclocalconfigurationmanager()]
+configuration lcm {
+    Settings {
+        RefreshMode = 'Pull'
+    }
+
+    ConfigurationRepositoryWeb SQLPullWeb {
+        ServerURL = 'http://wds01:8080/PSDSCPullServer.svc'
+        RegistrationKey = 'cb30127b-4b66-4f83-b207-c4801fb05087'
+        AllowUnsecureConnection = $true
+    }
+
+    ReportServerWeb SQLPullWeb {
+        ServerURL = 'http://wds01:8080/PSDSCPullServer.svc'
+        RegistrationKey = 'cb30127b-4b66-4f83-b207-c4801fb05087'
+        AllowUnsecureConnection = $true
+    }
+}
+
+lcm -OutputPath C:\Windows\temp
+Set-DscLocalConfigurationManager c:\windows\temp -Verbose
