12bcb1f8d9c5b90add133d099a7b0e988e09ca49
Jur <jorgen.de.gier@salomon-it.nl>
2020-09-29 21:39:44 +0200

    create unattend.xml

--- InstallWDS.ps1
+++ InstallWDS.ps1
@@ -41,5 +41,174 @@ if( $win32cs.Manufacturer -eq 'Xen')
     Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow
 }
 
+$xmlunattend = [xml]'<unattend xmlns="urn:schemas-microsoft-com:unattend">
+            <settings pass="windowsPE">
+                <component name="Microsoft-Windows-International-Core-WinPE" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                    <SetupUILanguage>
+                        <UILanguage>en-US</UILanguage>
+                    </SetupUILanguage>
+                    <SystemLocale>en-US</SystemLocale>
+                    <UILanguage>en-US</UILanguage>
+                    <UserLocale>en-US</UserLocale>
+                </component>
+                <component name="Microsoft-Windows-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                   <Diagnostics>
+                        <OptIn>false</OptIn>
+                    </Diagnostics>
+                    <DiskConfiguration>
+                        <WillShowUI>OnError</WillShowUI>
+                        <Disk wcm:action="add">
+                            <DiskID>0</DiskID>
+                            <WillWipeDisk>true</WillWipeDisk>
+                            <CreatePartitions>
+                                <CreatePartition wcm:action="add">
+                                    <Order>1</Order>
+                                    <Size>100</Size>
+                                    <Type>EFI</Type>
+                                </CreatePartition>
+                                <CreatePartition wcm:action="add">
+							        <Order>2</Order> 
+							        <Type>MSR</Type> 
+							        <Size>128</Size> 
+                                </CreatePartition>
+                                <CreatePartition wcm:action="add">
+							        <Order>3</Order> 
+							        <Type>Primary</Type> 
+							        <Extend>true</Extend> 
+                                </CreatePartition>
+                            </CreatePartitions>
+                            <ModifyPartitions>
+                                <ModifyPartition wcm:action="add">
+							        <Order>1</Order> 
+							        <PartitionID>1</PartitionID> 
+							        <Label>System</Label> 
+							        <Format>FAT32</Format> 
+						        </ModifyPartition>
+						        <ModifyPartition wcm:action="add">
+							        <Order>2</Order> 
+							        <PartitionID>3</PartitionID> 
+							        <Label>Local Disk</Label> 
+							        <Letter>C</Letter> 
+							        <Format>NTFS</Format> 
+						        </ModifyPartition>
+					        </ModifyPartitions>
+                        </Disk>
+                    </DiskConfiguration>
+                    <ImageInstall>
+                        <OSImage>
+                            <InstallTo>
+                                <DiskID>0</DiskID>
+                                <PartitionID>3</PartitionID>
+                            </InstallTo>
+                            <WillShowUI>OnError</WillShowUI>
+                            <InstallToAvailablePartition>false</InstallToAvailablePartition>
+                        </OSImage>
+                    </ImageInstall>
+                    <UserData>
+                        <AcceptEula>true</AcceptEula>
+                        <FullName></FullName>
+                        <Organization></Organization>
+                        <ProductKey>
+                            <WillShowUI>Never</WillShowUI>
+                        </ProductKey>
+                    </UserData>
+                    <EnableFirewall>true</EnableFirewall>
+                    <EnableNetwork>true</EnableNetwork>
+                    <WindowsDeploymentServices>
+                        <Login>
+                            <Credentials>
+                                <Username></Username>
+                                <Password></Password>
+                                <Domain></Domain>
+                            </Credentials>
+                        </Login>
+                        <ImageSelection>
+                            <InstallImage>
+                                <ImageName></ImageName>
+                                <ImageGroup></ImageGroup>
+                            </InstallImage>
+                            <InstallTo>
+                                <DiskID>0</DiskID>
+                                <PartitionID>3</PartitionID>
+                            </InstallTo>
+                        </ImageSelection>
+                    </WindowsDeploymentServices>
+                </component>
+            </settings>
+            <settings pass="generalize">
+                <component name="Microsoft-Windows-Security-SPP" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                    <SkipRearm>1</SkipRearm>
+                </component>
+                <component name="Microsoft-Windows-PnpSysprep" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                    <PersistAllDeviceInstalls>true</PersistAllDeviceInstalls>
+                </component>
+            </settings>
+            <settings pass="specialize">
+                <component name="Microsoft-Windows-Security-SPP-UX" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                    <SkipAutoActivation>true</SkipAutoActivation>
+                </component>
+                <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                    <TimeZone>GMT Standard Time</TimeZone>
+                    <ComputerName>TEMPLATE</ComputerName>
+                </component>
+                <component name="Microsoft-Windows-TerminalServices-LocalSessionManager" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                    <fDenyTSConnections>false</fDenyTSConnections>
+                </component>
+                <component name="Microsoft-Windows-TerminalServices-RDP-WinStationExtensions" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                    <UserAuthentication>0</UserAuthentication>
+                </component>
+                <component name="Microsoft-Windows-IE-ESC" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                    <IEHardenAdmin>false</IEHardenAdmin>
+                    <IEHardenUser>false</IEHardenUser>
+                </component>
+                <component name="Microsoft-Windows-ServerManager-SvrMgrNc" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                    <DoNotOpenServerManagerAtLogon>true</DoNotOpenServerManagerAtLogon>
+                </component>
+                <component name="Networking-MPSSVC-Svc" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                    <FirewallGroups>
+                        <FirewallGroup wcm:action="add" wcm:keyValue="rd1">
+                            <Profile>all</Profile>
+                            <Active>true</Active>
+                            <Group>Remote Desktop</Group>
+                        </FirewallGroup>
+                    </FirewallGroups>
+                </component>
+            </settings>
+            <settings pass="oobeSystem">
+                <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                    <UserAccounts>
+                        <AdministratorPassword>
+                            <Value>MQAyAHcAcQAhAEAAVwBRAEEAZABtAGkAbgBpAHMAdAByAGEAdABvAHIAUABhAHMAcwB3AG8AcgBkAA==</Value>
+                            <PlainText>false</PlainText>
+                        </AdministratorPassword>
+                    </UserAccounts>
+                    <OOBE>
+                        <HideEULAPage>true</HideEULAPage>
+                        <SkipMachineOOBE>true</SkipMachineOOBE>
+                        <SkipUserOOBE>true</SkipUserOOBE>
+                        <NetworkLocation>Work</NetworkLocation>
+                        <ProtectYourPC>3</ProtectYourPC>
+                        <HideWirelessSetupInOOBE>true</HideWirelessSetupInOOBE>
+                    </OOBE>
+                    <TimeZone>W. Europe Standard Time</TimeZone>
+                    <DisableAutoDaylightTimeSet>false</DisableAutoDaylightTimeSet>
+                </component>
+            </settings>
+        </unattend>'
+$winpe = $xmlunattend.unattend.settings | Where-Object{ $_.pass -eq 'windowsPE' }
+$winpe.component.Where( {$_.name -eq 'Microsoft-Windows-International-Core-WinPE'} )
+$winpe.component.Where( {$_.name -eq 'Microsoft-Windows-Setup'} ).WindowsDeploymentServices.Login.Credentials.Username = 'administrator'
+$winpe.component.Where( {$_.name -eq 'Microsoft-Windows-Setup'} ).WindowsDeploymentServices.Login.Credentials.Password = '12wq!@WQ'
+$winpe.component.Where( {$_.name -eq 'Microsoft-Windows-Setup'} ).WindowsDeploymentServices.Login.Credentials.Domain = 'wds01'
+
+#$imagedetail = $this.GetImages().Where( { $_.Name -eq $image } )
+#$winpe.component.Where( {$_.name -eq 'Microsoft-Windows-Setup'} ).WindowsDeploymentServices.ImageSelection.InstallImage.ImageName = $image
+#$winpe.component.Where( {$_.name -eq 'Microsoft-Windows-Setup'} ).WindowsDeploymentServices.ImageSelection.InstallImage.ImageGroup = $imagedetail.ImageGroup
+
+#$spec = $xml.unattend.settings | where{ $_.pass -eq 'specialize' }
+#$spec.component.Where( {$_.name -eq 'Microsoft-Windows-Shell-Setup'} )[0].ComputerName = $vmname
+
+$xmlunattend.Save( "c:\windows\temp\unattend.xml" )
+
 Invoke-WebRequest -Uri https://github.com/JorgendG/BuildWDS/raw/master/PullServerSQL.ps1 -OutFile $env:TEMP\PullServerSQL.ps1
 & $env:TEMP\PullServerSQL.ps1
