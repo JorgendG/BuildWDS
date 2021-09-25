7fc5cbfd5d7134cb5489e86bde814d0ca01f564f
Jorgen de Gier <jwdegier@wxs.nl>
2021-09-25 04:37:40 +0200

    xenagent eruit, pfx thumbprint lezen

--- InstallWDS.ps1
+++ InstallWDS.ps1
@@ -2,8 +2,6 @@
 # oscdimg.exe -m -o -u2 -udfver102 -bootdata:2#p0,e,bc:\temp\iso\boot\etfsboot.com#pEF,e,bc:\temp\iso\efi\microsoft\boot\efisys.bin c:\temp\iso c:\temp\wds01.iso
 start-transcript -path c:\windows\temp\installwds.txt
 
-$sourcediragents = "\\hyperdrive\public\agents"
-
 #[Net.ServicePointManager]::SecurityProtocol='tls12,tls11,tls'
 #Install-Module CertificateDsc -Force
 # Argh, install-module klapt eruit omdat er nog geen default profiel is
@@ -15,41 +13,7 @@ $sourcediragents = "\\hyperdrive\public\agents"
 # Onderstaande code via een eenmalige(?) scheduled task uitvoeren die heel snel volgt op het huidige moment. Als setupcomplete.cmd is 
 # afgerond is er vast een default profile waardoor install-module wel werkt
 
-#Copy-Item $sourcedirmodules\xPSDesiredStateConfiguration 'C:\Program Files\WindowsPowerShell\Modules' -Recurse -Force
-#Copy-Item $sourcedirmodules\SqlServerDsc 'C:\Program Files\WindowsPowerShell\Modules' -Recurse -Force
-#Copy-Item $sourcedirmodules\cWDS 'C:\Program Files\WindowsPowerShell\Modules' -Recurse -Force
-#Copy-Item $sourcedirmodules\xPendingReboot 'C:\Program Files\WindowsPowerShell\Modules' -Recurse -Force
-#Copy-Item $sourcediragents\managementagentx64.msi 'C:\Windows\Temp' -Recurse -Force
-
-#$cred = Get-Credential
-$sourcePath = "$sourcediragents\managementagentx64.msi"
-$destPath = "C:\Windows\Temp\managementagentx64.msi"
-
-$SharePwd = "P@ssword!" | ConvertTo-SecureString -AsPlainText -Force
-$ShareUserName = "hyperdrive\readonly"
-$ShareCredentials = New-Object System.Management.Automation.PSCredential -ArgumentList $ShareUserName, $SharePwd
-
-Start-BitsTransfer -Source $sourcePath -Destination $destPath -Credential $ShareCredentials
-
-
-#Install-PackageProvider -Name "Nuget" -Force
-#Register-PackageSource -Name chocolatey -Location http://chocolatey.org/api/v2 -ProviderName NuGet -Trusted -Verbose
-#Install-Package -Name sql-server-management-studio -ProviderName chocolatey -force
-
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 2
-
-$win32cs = Get-WmiObject -query 'select * from Win32_ComputerSystem'
-
-if( $win32cs.Manufacturer -eq 'Xen')
-{
-    $MSIArguments = @(
-        "/i"
-        ('"{0}"' -f "c:\windows\temp\managementagentx64.msi")
-        "/qn"
-        "/norestart"
-    )
-    Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow
-}
 
 $xmlunattend = [xml]'<unattend xmlns="urn:schemas-microsoft-com:unattend">
             <settings pass="windowsPE">
