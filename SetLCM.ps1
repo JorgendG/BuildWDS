7a2d93ef49a348866a2373d70fa29c8524abd293
Jorgen de Gier <jwdegier@wxs.nl>
2021-09-25 04:38:08 +0200

    pfx thumprint

--- SetLCM.ps1
+++ SetLCM.ps1
@@ -3,7 +3,7 @@ Get-LocalUser -Name "Administrator" | Enable-LocalUser
 # certificaat tbv credential encryptie
 Invoke-WebRequest -Uri http://wds01/DscPrivatePublicKey.pfx.txt -OutFile C:\Windows\Temp\DscPrivatePublicKey.pfx
 $mypwd = ConvertTo-SecureString -String "1234" -Force -AsPlainText
-Import-PfxCertificate -FilePath C:\Windows\Temp\DscPrivatePublicKey.pfx -Password $mypwd -CertStoreLocation Cert:\LocalMachine\My
+$pfx = Import-PfxCertificate -FilePath C:\Windows\Temp\DscPrivatePublicKey.pfx -Password $mypwd -CertStoreLocation Cert:\LocalMachine\My
 
 Invoke-WebRequest -Uri http://wds01/wds01.cer.txt -OutFile C:\Windows\Temp\wds01.cer
 Import-Certificate -FilePath C:\Windows\Temp\wds01.cer -CertStoreLocation Cert:\LocalMachine\Root
@@ -17,7 +17,7 @@ configuration lcm {
         RefreshFrequencyMins = 30
         RebootNodeIfNeeded = $true
         ConfigurationMode = 'ApplyAndAutoCorrect'
-        CertificateID = '05daa37d7a0013e346dc0fd0350da79c3193a4ab'
+        CertificateID = $pfx.Thumbprint
     }
 
     ConfigurationRepositoryWeb SQLPullWeb {
@@ -45,3 +45,4 @@ lcm -OutputPath C:\Windows\temp
 Set-Service -Name winrm -StartupType Automatic
 Start-Service winrm
 Set-DscLocalConfigurationManager c:\windows\temp -Verbose
+Update-DscConfiguration
