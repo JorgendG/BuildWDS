445ecee87acec34b3f21f09e3d71284c41ff0971
Jorgen de Gier <jwdegier@wxs.nl>
2022-11-16 01:55:22 +0100

    als service

--- WebhookListener.ps1
+++ WebhookListener.ps1
@@ -1,3 +1,15 @@
+<#
+$nssm = 'c:\pullserver\nssm.exe'
+$serviceName = 'GitHubWebHook'
+$powershell = (Get-Command powershell).Source
+$scriptPath = 'C:\pullserver\githubseintje.ps1'
+$arguments = '-ExecutionPolicy Bypass -NoProfile -File "{0}"' -f $scriptPath
+& $nssm install $serviceName $powershell $arguments
+& $nssm status $serviceName
+Start-Service $serviceName
+Get-Service $serviceName
+#>
+
 Add-Type -AssemblyName System.Web
 
 $HttpListener = New-Object System.Net.HttpListener
@@ -5,12 +17,14 @@ $HttpListener.Prefixes.Add("http://+:1234/")
 
 $HttpListener.Start()
 While ($HttpListener.IsListening) {
+    $getMakeDSCConfigps1 = $false
+    $getMakeDSCConfigpsd1 = $false
+
     $HttpContext = $HttpListener.GetContext()
     $HttpRequest = $HttpContext.Request
     $RequestUrl = $HttpRequest.Url.OriginalString
     Write-Output "$RequestUrl"
-    if($HttpRequest.HasEntityBody) 
-    {
+    if ($HttpRequest.HasEntityBody) {
         $Reader = New-Object System.IO.StreamReader($HttpRequest.InputStream)
         $bla = $Reader.ReadToEnd()
         $decodedpayload = [System.Web.HttpUtility]::UrlDecode($bla)
@@ -21,13 +35,11 @@ While ($HttpListener.IsListening) {
 
         Write-Output "Files modified:"
         $whevent[0].head_commit.modified
-        if( 'MakeDSCConfig.ps1' -in $whevent[0].head_commit.modified )
-        {
-            Invoke-WebRequest -Uri https://github.com/JorgendG/BuildWDS/raw/master/MakeDSCConfig.ps1 -OutFile C:\Pullserver\MakeDSCConfig.ps1
+        if ( 'MakeDSCConfig.ps1' -in $whevent[0].head_commit.modified ) {
+            $getMakeDSCConfigps1 = $true
         }
-        if( 'MakeDSCConfig.psd1' -in $whevent[0].head_commit.modified )
-        {
-            Invoke-WebRequest -Uri https://github.com/JorgendG/BuildWDS/raw/master/MakeDSCConfig.psd1 -OutFile C:\Pullserver\MakeDSCConfig.psd1
+        if ( 'MakeDSCConfig.psd1' -in $whevent[0].head_commit.modified ) {
+            $getMakeDSCConfigpsd1 = $true
         }
         Write-Output "Files added:"
         $whevent[0].head_commit.added
@@ -35,13 +47,20 @@ While ($HttpListener.IsListening) {
         #Write-Output $decodedpayload
     }
     $HttpResponse = $HttpContext.Response
-    $HttpResponse.Headers.Add("Content-Type","text/plain")
+    $HttpResponse.Headers.Add("Content-Type", "text/plain")
     $HttpResponse.StatusCode = 200
     $ResponseBuffer = [System.Text.Encoding]::UTF8.GetBytes("")
     $HttpResponse.ContentLength64 = $ResponseBuffer.Length
-    $HttpResponse.OutputStream.Write($ResponseBuffer,0,$ResponseBuffer.Length)
+    $HttpResponse.OutputStream.Write($ResponseBuffer, 0, $ResponseBuffer.Length)
     $HttpResponse.Close()
     Write-Output "" # Newline
     #$HttpListener.Stop()
+    if ( $getMakeDSCConfigps1 ) {
+        Invoke-WebRequest -Uri https://github.com/JorgendG/BuildWDS/raw/master/MakeDSCConfig.ps1 -OutFile C:\Pullserver\MakeDSCConfig.ps1
+    }
+    if ( $getMakeDSCConfigpsd1 ) {
+        Invoke-WebRequest -Uri https://github.com/JorgendG/BuildWDS/raw/master/MakeDSCConfig.psd1 -OutFile C:\Pullserver\MakeDSCConfig.psd1
+    }
+    # trap makeconfig af
 }
 $HttpListener.Stop()
