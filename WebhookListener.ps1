976c83cf730c602a4d548b201767a5a1a38ce71d
Jorgen de Gier <jwdegier@wxs.nl>
2022-11-19 04:58:30 +0100

    x

--- WebhookListener.ps1
+++ WebhookListener.ps1
@@ -1,20 +1,39 @@
+
 <#
-$nssm = 'c:\pullserver\nssm.exe'
-$serviceName = 'GitHubWebHook'
-$powershell = (Get-Command powershell).Source
-$scriptPath = 'C:\pullserver\githubseintje.ps1'
-$arguments = '-ExecutionPolicy Bypass -NoProfile -File "{0}"' -f $scriptPath
-& $nssm install $serviceName $powershell $arguments
-& $nssm status $serviceName
-Start-Service $serviceName
-Get-Service $serviceName
+    # Heb credentials nodig om logon voor service in te stellen
+    $credpwd = Get-Content c:\Windows\Temp\credpwd.txt | ConvertTo-SecureString
+    $usr = Get-Content c:\Windows\Temp\credusr.txt
+    $credential = New-Object System.Management.Automation.PsCredential($usr, $credpwd)
+    $usr
+    $credential.Password
+
+    $credential.Password | Out-File C:\pullserver\dinges.txt
 #>
+
+<#
+$serviceName = 'GitHubWebHook'
+$ghwbservice = Get-Service $serviceName -ErrorAction SilentlyContinue
+if( $null -eq $ghwbservice )
+{
+    $nssm = 'c:\pullserver\nssm.exe'
+    $serviceName = 'GitHubWebHook'
+    $powershell = (Get-Command powershell).Source
+    $scriptPath = 'C:\pullserver\githubseintje.ps1'
+    $arguments = '-ExecutionPolicy Bypass -NoProfile -File "{0}"' -f $scriptPath
+    & $nssm install $serviceName $powershell $arguments
+    & $nssm status $serviceName
+    Start-Service $serviceName
+    Get-Service $serviceName
+}
+#>
+
 
 
 Add-Type -AssemblyName System.Web
 
 $sourcerepo = 'https://github.com/JorgendG/BuildWDS/raw/master'
 $destFolder = 'C:\Pullserver'
+$updateaction = 'C:\Pullserver\MakeDSCConfig.ps1'
 
 $HttpListener = New-Object System.Net.HttpListener
 $HttpListener.Prefixes.Add("http://+:1234/")
@@ -69,7 +88,8 @@ While ($HttpListener.IsListening) {
     if ( (Test-Path C:\pullserver\HomelabConfig\credpwd.txt) -and (Test-Path C:\pullserver\HomelabConfig\credusr.txt) ) {
         if ( $getMakeDSCConfigps1 -or $getMakeDSCConfigpsd1 ) {
             Write-Output "Start MakeDSCConfig" $((get-date).ToLocalTime()).ToString("yyyy-MM-dd HHmmss")
-            & C:\Pullserver\MakeDSCConfig.ps1
+            #& C:\Pullserver\MakeDSCConfig.ps1
+            & "$updateaction"
             Write-Output "Einde MakeDSCConfig" $((get-date).ToLocalTime()).ToString("yyyy-MM-dd HHmmss")
         }
     }
